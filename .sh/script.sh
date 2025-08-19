8ab4927f6530ed9127d8681755525e3145e6e0c8
V Kuznetsov <vvsmith13@gmail.com>
2025-08-19 19:14:05 +0300

    A1.00

---
+++ .sh/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cmnt="A1.00"
+git add --all
+git commit -m $cmnt
+git push
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = '01';
+  // private _formBuilder = inject(FormBuilder);
+
+  // firstFormGroup = this._formBuilder.group({
+  //   firstCtrl: ['', Validators.required],
+  // });
+  // secondFormGroup = this._formBuilder.group({
+  //   secondCtrl: ['', Validators.required],
+  // });
+  // isLinear = false;
 }
